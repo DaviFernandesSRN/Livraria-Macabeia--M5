48ee949466a0171230bcac6c4bb4d4de5ddcb3fd
Amanda Alexandre <amanda.alexandre@outlook.com>
2022-06-01 11:28:45 -0300

    feat: creating header component

---
+++ Livraria Macabéa Site/src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import '../style/header.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser, faCartShopping, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+
+function Header () {
+    return (
+        <nav className='top'>
+            <img src="src\img\logo.png" className='logo' />
+            <div className='rightbar'>
+                <div className='search-bar'>
+                    <FontAwesomeIcon icon={faMagnifyingGlass} className="icon" />
+                    &nbsp;
+                    Procure por mais de XX livros
+                </div>
+                <div className='icons'>
+                    <FontAwesomeIcon icon={faCartShopping} className="icon" />
+                    <FontAwesomeIcon icon={faUser} className="icon" />
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Header;
