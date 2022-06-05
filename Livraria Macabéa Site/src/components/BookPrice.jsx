f35543bf767ec6bc72ff39d47eae95a853be353f
Amanda Alexandre <amanda.alexandre@outlook.com>
2022-06-05 15:35:30 -0300

    style: book details

--- Livraria Macabéa Site/src/components/BookPrice.jsx
+++ Livraria Macabéa Site/src/components/BookPrice.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function BookPrice () {
-    return (<h4>10.0</h4>)
+    return (<h4>R$ 10.0</h4>)
 }
 
 export default BookPrice;
--- Livraria Macabéa Site/src/components/BookTitle.jsx
+++ Livraria Macabéa Site/src/components/BookTitle.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function BookTitle () {
-    return (<h1>Titulo do livro</h1>)
+    return (<h2>Titulo do livro</h2>)
 }
 
 export default BookTitle;
