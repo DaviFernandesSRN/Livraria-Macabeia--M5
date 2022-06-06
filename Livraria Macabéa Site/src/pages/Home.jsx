bdd1699b1a3125e6cd8e178dc1cd046015965cc0
Amanda Alexandre <amanda.alexandre@outlook.com>
2022-06-06 12:10:39 -0300

    feat: homepage top banner

--- Livraria Macabéa Site/src/pages/Home.jsx
+++ Livraria Macabéa Site/src/pages/Home.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import '../assets/style/home.css'
+import '../assets/style/button.css'
 
 function Home() {
     return (
-        <div className='container'>
+        // <div className='container'>
             <section className='banner-top'>
-                <div class="message-top">
-                    adskjdahk
+                <div className='message-top'>
+                    <h4>FESTIVAL DE LEITURA</h4>
+                    <h1>SELEÇÃO DE LIVROS ESPECIAIS</h1>
+                    <h2>para fãs de literatura</h2>
+                    <br/>
+                    <button>APROVEITE</button>
                 </div>
                 <img src='src\assets\img\img-topo-red.png' className='img-top' />
             </section>
 
-        </div>
+        // </div>
         
     )
 }
