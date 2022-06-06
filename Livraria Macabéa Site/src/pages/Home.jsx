520c45f1c00814332db0badd9e78574ab4c07100
Amanda Alexandre <amanda.alexandre@outlook.com>
2022-06-06 11:42:25 -0300

    style: animation

--- Livraria Macabéa Site/src/pages/Home.jsx
+++ Livraria Macabéa Site/src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
-import '../assets/style/'
+import '../assets/style/home.css'
 
 function Home() {
     return (
         <div className='container'>
-            <h1>Home</h1>
+            <section className='banner-top'>
+                <div class="message-top">
+                    adskjdahk
+                </div>
+                <img src='src\assets\img\img-topo-red.png' className='img-top' />
+            </section>
 
         </div>
         
