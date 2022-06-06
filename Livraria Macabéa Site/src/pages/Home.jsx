b358b07f950a432c44d3faa045e51107e261a0f3
Amanda Alexandre <amanda.alexandre@outlook.com>
2022-06-06 13:01:17 -0300

    feat: homepage cards

--- Livraria Macabéa Site/src/pages/Home.jsx
+++ Livraria Macabéa Site/src/pages/Home.jsx
@@ -1,22 +1,13 @@
 import React from 'react'
-import '../assets/style/home.css'
-import '../assets/style/button.css'
+import BannerTop from '../components/BannerTop'
+import TopCards from '../components/TopCards'
 
 function Home() {
     return (
-        // <div className='container'>
-            <section className='banner-top'>
-                <div className='message-top'>
-                    <h4>FESTIVAL DE LEITURA</h4>
-                    <h1>SELEÇÃO DE LIVROS ESPECIAIS</h1>
-                    <h2>para fãs de literatura</h2>
-                    <br/>
-                    <button>APROVEITE</button>
-                </div>
-                <img src='src\assets\img\img-topo-red.png' className='img-top' />
-            </section>
-
-        // </div>
+        <div>
+            <BannerTop />
+            <TopCards />
+        </div>
         
     )
 }
