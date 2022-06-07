36dcd1846151f3c333a6fa43a45d57963d75e6f7
Amanda Alexandre <amanda.alexandre@outlook.com>
2022-06-07 16:47:43 -0300

    style: search box positioning

--- Livraria Macabéa Site/src/components/Header.jsx
+++ Livraria Macabéa Site/src/components/Header.jsx
@@ -3,7 +3,7 @@ import '../assets/style/header.css'
 import { Link } from 'react-router-dom'
 import GetLivrosPor from '../utils/getLivrosPor'
 import axios from 'axios'
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 
 function Header () {
     const[ilivros, setIlivros] = useState('')
@@ -12,35 +12,30 @@ function Header () {
     return (
         <header className='top'>
             <img src="src\assets\img\logo.png" className='logo' />
+
             <div className='rightbar'>
-              
-                <input type="text" value={ilivros} onChange={(e)=>setIlivros(e.target.value)} className='search-bar' placeholder={MensagemCaixaDeBusca} />
-                    <img src="src\assets\img\search.png" className="icon" />
-                    &nbsp;
-                    Procure por mais de 10 livros
+                <div className='search-bar' >
+                    <input type="text" value={ilivros} onChange={(e)=>setIlivros(e.target.value)} placeholder={MensagemCaixaDeBusca} className='inputbox' />
+                    <Link to={`/procurar/${ilivros}`} > 
+                        <img src="src\assets\img\search.png" className="icon icon-search-box" />
+                    </Link>
+                    
+                </div>   
              
-                
-                <div className='icons'>
-                
+                {/*Ícones  */}            
                     <img src="src\assets\img\search.png" className="icon" id="search-toggle"/>
                 
-
                 <Link to="/carrinho">
                     <img src="src\assets\img\shopping-cart.png" className="icon" />
                 </Link>
+
                 <Link to='/conta'>
                     <img src="src\assets\img\account.png" className="icon" />
                 </Link>
-                <Link to='/Cadastro'>
+                {/* <Link to='/Cadastro'>
                     Cadastro
-                </Link>
-
-
-                </div>
-            </div>
-
-            
-             <Link to={`/procurar/${ilivros}`} > pesquisar</Link>
+                </Link> */}
+            </div>  
         </header>
     )
 }
