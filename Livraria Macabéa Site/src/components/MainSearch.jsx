1c8d9f2b1e7efd3fd47c661ca39d1ee6e01b1dfa
Amanda Alexandre <amanda.alexandre@outlook.com>
2022-06-08 07:31:40 -0300

    style: main search page

--- Livraria Macabéa Site/src/components/MainSearch.jsx
+++ Livraria Macabéa Site/src/components/MainSearch.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import {useState} from 'react';
 import { Link } from 'react-router-dom'
+import '../assets/style/main-search.css'
 
 function MainSearch() {
     const[ilivros, setIlivros] = useState('')
-    const MessageSearchBox = 'Procure um livro pelo nome'
 
     return (
         <div className="busca-container">
-            <input type="text" onChange={(e)=>setIlivros(e.target.value)} className='searchbox' placeholder={MessageSearchBox} />
 
-            <Link to={`/procurar/${ilivros}`} > <img src="src\assets\img\search.png" className="icon"  /></Link>
+            <h1>Busque o livro pelo seu nome</h1>
+
+            <span>
+                <input type="text" onChange={(e)=>setIlivros(e.target.value)} className='mainsearchbox' />
+
+                <Link to={`/procurar/${ilivros}`} > 
+                <button>PROCURAR</button>
+                </Link>
+
+            </span>
         </div>
     )
 }
