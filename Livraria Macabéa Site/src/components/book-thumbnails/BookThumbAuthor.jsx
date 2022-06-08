1dcd7991efec062e15be6a4e11b56fadb2414f44
Davi Fernandes <davifernandessrn@gmail.con>
2022-06-08 16:47:31 -0300

    deletando arquivos obsoletos

--- Livraria Macabéa Site/src/components/book-thumbnails/BookThumbAuthor.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import '../../assets/style/book-thumb.css'
-
-function BookAuthor () {
-    return (<p className="book-author">Autor: Autor do livro</p>)
-} 
-
-export default BookAuthor;
